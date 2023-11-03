321d97d1bfb2db86b1795fddbc6ac0c23cc9827c
HoangHai0156 <hoanghai41099@gmail.com>
2023-11-03 11:54:06 +0700

    first commit

---
+++ constant/content.js
@@ -0,0 +1,4 @@
+export const ENG = {
+  about:
+    "Passionate Java Developer with a Bachelor's degree from Hue University of Foreign Languages (English major) and a successful transition into programming through self-guided study and intensive coding boot camp training. Equipped with logical thinking, critical-thinking linguistic finesse, and a robust grasp of software development, I'm excited to apply my problem-solving abilities and contribute effectively to a dynamic team.",
+};
